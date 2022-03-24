a7cebb91d8eb62a984e149e045248f92e5b5045f
weicheng lee <z82111712@gmail.com>
2022-03-24 15:51:02 -0700

    update readme on reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useReducer, useEffect } from 'react';
+
+const ACTION = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+  RESET: 'reset',
+  STORED: 'store',
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREMENT:
+      return { count: state.count + 1 }
+    case ACTION.DECREMENT:
+      return { count: state.count - 1 }
+    case ACTION.RESET:
+      return { count: 0 }
+    case ACTION.STORED:
+      return { count: state.count }
+    default:
+      return state;
+  }
+}
+
+const LOCAL_STORAGE_KEY = 'testUseEffect'
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+  useEffect(() => {
+    const storedCount = localStorage.getItem(LOCAL_STORAGE_KEY);
+    // state.count = Number(storedCount);
+    state.count = Number(storedCount)
+    dispatch({type: ACTION.STORED})
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, state.count)
+  }, [state.count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: ACTION.INCREMENT })}>+</button>
+      <button onClick={() => dispatch({ type: ACTION.DECREMENT })}>-</button>
+      <button onClick={() => dispatch({ type: ACTION.RESET })}>RESET</button>
     </div>
   );
 }
